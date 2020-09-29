2832e5225901e96a96403182c44126a4d843762e
Matheus Rocco <matheus.rocco@quintoandar.com.br>
2020-09-29 17:52:51 -0300

    add new autocomplete and place details APIs implementation

--- flutter_google_places_sdk/android/src/main/kotlin/com/msh/flutter_google_places_sdk/FlutterGooglePlacesSdkPlugin.kt
+++ flutter_google_places_sdk/android/src/main/kotlin/com/msh/flutter_google_places_sdk/FlutterGooglePlacesSdkPlugin.kt
@@ -2,11 +2,16 @@ package com.msh.flutter_google_places_sdk
 
 import android.content.Context
 import androidx.annotation.NonNull
+import com.google.android.gms.maps.model.LatLng
 import com.google.android.libraries.places.api.Places
 import com.google.android.libraries.places.api.model.AutocompletePrediction
+import com.google.android.libraries.places.api.model.AutocompleteSessionToken
+import com.google.android.libraries.places.api.model.Place
 import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest
 import com.google.android.libraries.places.api.net.FindAutocompletePredictionsResponse
 import com.google.android.libraries.places.api.net.PlacesClient
+import com.google.android.libraries.places.api.net.FetchPlaceRequest
+import com.google.android.libraries.places.api.net.FetchPlaceResponse
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
@@ -16,13 +21,13 @@ import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 import java.util.*
 
-
 /** FlutterGooglePlacesSdkPlugin */
 class FlutterGooglePlacesSdkPlugin : FlutterPlugin, MethodCallHandler {
 
   private lateinit var client: PlacesClient
   private lateinit var channel: MethodChannel
   private lateinit var applicationContext: Context
+  private var token = AutocompleteSessionToken.newInstance()
 
   override fun onAttachedToEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
     onAttachedToEngine(binding.applicationContext, binding.binaryMessenger)
@@ -37,40 +42,89 @@ class FlutterGooglePlacesSdkPlugin : FlutterPlugin, MethodCallHandler {
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
     when (call.method) {
-      METHOD_INITIALIZE -> {
-        val apiKey = call.argument<String>("apiKey")
-        val localeMap = call.argument<Map<String, Any>>("locale")
-        val locale = readLocale(localeMap)
-        initialize(apiKey, locale)
-        result.success(null)
+      METHOD_INITIALIZE -> initialize(call, result)
+      METHOD_DEINITIALIZE -> deinitialize(call, result)
+      METHOD_IS_INITIALIZE -> isInitialized(call, result)
+      METHOD_FIND_AUTOCOMPLETE_PREDICTIONS -> autocompletePredictions(call, result)
+      METHOD_FETCH_PLACE -> fetchPlaceDetails(call, result)
+      else -> result.notImplemented()
+    }
+  }
+
+  private fun initialize(@NonNull call: MethodCall, @NonNull result: Result) {
+    val apiKey = call.argument<String>("apiKey")
+    val localeMap = call.argument<Map<String, Any>>("locale")
+    val locale = readLocale(localeMap)
+    initialize(apiKey, locale)
+    result.success(null)
+  }
+
+  private fun deinitialize(@NonNull call: MethodCall, @NonNull result: Result) {
+    Places.deinitialize()
+    result.success(null)
+  }
+
+  private fun isInitialized(@NonNull call: MethodCall, @NonNull result: Result) {
+    result.success(Places.isInitialized())
+  }
+
+  private fun autocompletePredictions(@NonNull call: MethodCall, @NonNull result: Result) {
+    val requestBuilder = FindAutocompletePredictionsRequest.builder()
+
+    call.argument<List<String>>("countries")?.let { countries ->
+      requestBuilder.setCountries(countries)
+    }
+
+    call.argument<Boolean>("newSessionToken")?.let { newSessionToken ->
+      if (newSessionToken) {
+        token = AutocompleteSessionToken.newInstance()
       }
-      METHOD_DEINITIALIZE -> {
-        Places.deinitialize()
-        result.success(null)
+    }
+
+    call.argument<Map<String, Double?>>("origin")?.let { origin ->
+      val originLocation = LatLng(
+        origin["lat"] ?: 0.0,
+        origin["lng"] ?: 0.0
+      )
+
+      requestBuilder.setOrigin(originLocation)
+    }
+
+    val query = call.argument<String>("query")
+
+    var request = requestBuilder
+      .setSessionToken(token)
+      .setQuery(query)
+      .build()
+
+    client.findAutocompletePredictions(request).addOnCompleteListener { task ->
+      if (task.isSuccessful) {
+        val resultList = responseToList(task.result)
+        print("Result: $resultList");
+        result.success(resultList)
+      } else {
+        val exception = task.exception
+        result.error("API_ERROR", exception?.message ?: "Unknown exception", null)
       }
-      METHOD_IS_INITIALIZE -> {
-        result.success(Places.isInitialized())
-      }
-      METHOD_FIND_AUTOCOMPLETE_PREDICTIONS -> {
-        val query = call.argument<String>("query")
-        val countries = call.argument<List<String>>("countries") ?: emptyList()
-        val request = FindAutocompletePredictionsRequest.builder()
-                .setQuery(query)
-                .setCountries(countries)
-                .build()
-        client.findAutocompletePredictions(request).addOnCompleteListener { task ->
-          if (task.isSuccessful) {
-            val resultList = responseToList(task.result)
-            print("Result: $resultList");
-            result.success(resultList)
-          } else {
-            val exception = task.exception
-            result.error("API_ERROR", exception?.message ?: "Unknown exception", null)
-          }
-        }
-      }
-      else -> {
-        result.notImplemented()
+    }
+  }
+
+  private fun fetchPlaceDetails(@NonNull call: MethodCall, @NonNull result: Result) {
+    val placeId = call.argument<String>("placeId") ?: ""
+    val fieldsList = call.argument<List<String>>("fields") ?: emptyList()
+    val placeFields = fieldsList.map {
+      Place.Field.valueOf(it)
+    }
+
+    val request = FetchPlaceRequest.newInstance(placeId, placeFields)
+
+    client.fetchPlace(request).addOnCompleteListener { task ->
+      if (task.isSuccessful) {
+        val resultObj = placeDetailsToMap(task.result)
+        result.success(resultObj)
+      } else {
+        val exception = task.exception
+        result.error("API_ERROR", exception?.message ?: "Unknown exception", null)
       }
     }
   }
@@ -90,6 +144,15 @@ class FlutterGooglePlacesSdkPlugin : FlutterPlugin, MethodCallHandler {
             "primaryText" to result.getPrimaryText(null).toString(),
             "secondaryText" to result.getSecondaryText(null).toString(),
             "fullText" to result.getFullText(null).toString()
+    )
+  }
+
+  private fun placeDetailsToMap(result: FetchPlaceResponse?): Map<String, Any?> {
+    return mapOf(
+            "location" to mapOf(
+              "lat" to result?.getPlace()?.getLatLng()?.latitude,
+              "lng" to result?.getPlace()?.getLatLng()?.longitude
+            )
     )
   }
 
@@ -117,6 +180,7 @@ class FlutterGooglePlacesSdkPlugin : FlutterPlugin, MethodCallHandler {
     private const val METHOD_DEINITIALIZE = "deinitialize"
     private const val METHOD_IS_INITIALIZE = "isInitialized"
     private const val METHOD_FIND_AUTOCOMPLETE_PREDICTIONS = "findAutocompletePredictions"
+    private const val METHOD_FETCH_PLACE = "fetchPlace"
 
     const val CHANNEL_NAME = "plugins.msh.com/flutter_google_places_sdk"
 
